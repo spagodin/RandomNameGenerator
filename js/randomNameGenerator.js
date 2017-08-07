2a152685e61b12cab2f9ddf4562e008554701421
Scott Pagodin <seppy101@gmail.com>
2017-08-07 17:19:44 -0400

    Added html helper

--- js/randomNameGenerator.js
+++ js/randomNameGenerator.js
@@ -1,9 +1,29 @@
+let nameList = [];
+let html='';
 
 const randomNames = () => {
     for (let i=0; i<100; i++) {
         let randomNumberFirst = Math.floor(Math.random() * firstName.length) + 1;
-        let randomNumberLast = Math.floor(Math.random() * lastName.length )+ 1;
-        console.log(`${firstName[randomNumberFirst]} ${lastName[randomNumberLast]}`);
+        let randomNumberLast = Math.floor(Math.random() * lastName.length)+ 1;
+        nameList.push(`${firstName[randomNumberFirst]} ${lastName[randomNumberLast]}`);
     }
 }
- randomNames();
+
+console.log(nameList);
+
+function printList () {
+    let html = '<ul>';
+    for (let i = 0; i<nameList.length; i++){
+        html += `<li> ${nameList[i]} </li>`;
+    }
+    html += '</ul>'
+    return html;
+    //console.log(html);
+}
+randomNames();
+document.getElementById('stuff').innerHTML = printList();
+
+
+
+
+
