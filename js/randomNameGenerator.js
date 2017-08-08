5d2541f2eb17371ea3b1a5d56bd7934f54032b6d
Scott Pagodin <seppy101@gmail.com>
2017-08-07 20:34:13 -0400

    Refactor DRY

--- js/randomNameGenerator.js
+++ js/randomNameGenerator.js
@@ -1,27 +1,24 @@
 let nameList = [];
-let html='';
+
+const randomArrayIndex = array => Math.floor(Math.random() * array.length) + 1;
 
 const randomNames = () => {
-    for (let i=0; i<100; i++) {
-        let randomNumberFirst = Math.floor(Math.random() * firstName.length) + 1;
-        let randomNumberLast = Math.floor(Math.random() * lastName.length)+ 1;
-        nameList.push(`${firstName[randomNumberFirst]} ${lastName[randomNumberLast]}`);
+    for (let i = 0; i < 10; i++) {
+        nameList.push(`${firstName[randomArrayIndex(firstName)]} ${lastName[randomArrayIndex(lastName)]}`);
     }
-}
+};
 
-console.log(nameList);
-
-function printList () {
+const printList = listName => {
     let html = '<ul>';
-    for (let i = 0; i<nameList.length; i++){
-        html += `<li> ${nameList[i]} </li>`;
+    for (let i = 0; i<listName.length; i++) {
+        html += `<li> ${listName[i]} </li>`;
     }
     html += '</ul>'
     return html;
-    //console.log(html);
-}
+};
+
 randomNames();
-document.getElementById('stuff').innerHTML = printList();
+document.getElementById('randomNames').innerHTML = printList(nameList);
 
 
 
